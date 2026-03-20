1c9117388229f1f04282aed9c178fa5824076ab8
TimelessLich <simonmauhansen0408@gmail.com>
2026-03-20 10:25:44 +0100

    Summary added to Interfaces.cs

--- Custom-Bastions/Src/Interfaces.cs
+++ Custom-Bastions/Src/Interfaces.cs
@@ -1,10 +1,15 @@
 namespace CustomBastion;
 
+/// <summary>
+/// Prints the whole thing 
+/// </summary>
 public interface IPrintable
 {
     void Print();
 }
-
+/// <summary>
+/// Prints what would be used in the games display
+/// </summary>
 public interface IPrintableInline
 {
     void PrintInline();
